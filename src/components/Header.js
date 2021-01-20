78fc4868def8a3873591d65f3340ce3d6b25366c
Myagmarsuren <miner_miga@yahoo.com>
2021-01-21 01:47:04 +0800

    Finished version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
-const header = () => {
+const Header = ({ onAdd, showAdd }) => {
+  const location = useLocation();
+
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "grey" : "black"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
 
-export default header;
+export default Header;
--- src/components/Task.js
+++ src/components/Task.js
@@ -7,7 +7,7 @@ const Task = ({ task, onDelete, onToggle }) => {
       onDoubleClick={() => onToggle(task.id)}
     >
       <h3>
-        {task.text} <DeleteIcon onDelete={onDelete} />
+        {task.text} <DeleteIcon onClick={() => onDelete(task.id)} />
       </h3>
       <p>{task.day}</p>
     </div>
