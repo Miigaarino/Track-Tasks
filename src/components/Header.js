8295cb90179d905daf7e2c19795a1b08abae20bc
Myagmarsuren <miner_miga@yahoo.com>
2021-01-21 00:10:07 +0800

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+const header = () => {
+  return (
+    <header className="header">
+      <h1>Task Tracker</h1>
+      <Button />
+    </header>
+  );
+};
+
+export default header;
